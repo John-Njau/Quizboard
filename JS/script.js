38a363027a21d7bb0b75c606869e3c81d813032a
Johndevtech <john.njoroge@student.moringaschool.com>
2022-02-05 17:00:31 +0300

    This commit will add a function that calculates the percentage score

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,33 @@
 // This function stores the answer of the test questions
 function submitButton(){
     var score = 0;
-    var correctAnswer1 = document.first.question1.value;
-    var correctAnswer2 = document.first.question2.value;
-    var correctAnswer3 = document.first.question3.value;
-    var correctAnswer4 = document.first.question4.value;
-    var correctAnswer5 = document.first.question5.value;
-    var correctAnswer6 = document.first.question6.value;
-    var correctAnswer7 = document.first.question7.value;
-    var correctAnswer8 = document.first.question8.value;
-    if (correctAnswer1 == "B")(score +=10);
-    if (correctAnswer2 == "A")(score +=10);
-    if (correctAnswer3 == "C")(score +=10);
-    if (correctAnswer4 == "C")(score +=10);
-    if (correctAnswer5 == "B")(score +=10);
-    if (correctAnswer6 == "D")(score +=10);
-    if (correctAnswer7 == "D")(score +=10);
-    if (correctAnswer8 == "C")(score +=10);
-    document.write(score);
+    const correctAnswer1 = document.first.question1.value;
+    const correctAnswer2 = document.first.question2.value;
+    const correctAnswer3 = document.first.question3.value;
+    const correctAnswer4 = document.first.question4.value;
+    const correctAnswer5 = document.first.question5.value;
+    const correctAnswer6 = document.first.question6.value;
+    const correctAnswer7 = document.first.question7.value;
+    const correctAnswer8 = document.first.question8.value;
+    if (correctAnswer1 == "B")(score ++);
+    if (correctAnswer2 == "A")(score ++);
+    if (correctAnswer3 == "C")(score ++);
+    if (correctAnswer4 == "C")(score ++);
+    if (correctAnswer5 == "B")(score ++);
+    if (correctAnswer6 == "D")(score ++);
+    if (correctAnswer7 == "D")(score ++);
+    if (correctAnswer8 == "C")(score ++);
+    document.write(score + " answers correct out of 8.");
+    // function percentage(){
+
+    var scorePercent = (Math.floor(score * 100/8));
+    if (scorePercent >= 80){
+    document.write("Congratulations! You have scored " + scorePercent + "%.  EXCELLENT!!!");
+    } else if (scorePercent >= 50) {
+        document.write("Congratulations, You have scored " + scorePercent + "% . FAIR!");
+    } else {
+        document.write("You have scored " + scorePercent + "%.  FAILED. RETAKE THE TEST!");
+    }
 }
+
+
