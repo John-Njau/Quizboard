c63c42bfcecff9d36337581438be246cea0c866b
Johndevtech <john.njoroge@student.moringaschool.com>
2022-02-05 15:49:29 +0300

    ft:submit button function - This commit will create a submit function that tallys the users points

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+// This function stores the answer of the test questions
+function submitButton(){
+    var score = 0;
+    var correctAnswer = document.first.question1.value;
+    if (correctAnswer == "B")(score +=10)
+    document.write(score);
+}
